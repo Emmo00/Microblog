ff8924f8866df5bbb09718759577283e08fc34ec
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2024-01-01 01:40:30 +0100

    feat: Logging to file

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask_status import FlaskStatus
 from flask_sqlalchemy import SQLAlchemy
@@ -16,5 +20,15 @@ FlaskStatus(app)
 login = LoginManager(app)
 
 login.login_view = 'login'
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=1024, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 from app import routes, models, errors
