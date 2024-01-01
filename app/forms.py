86ada949150c2e8abfef8cd288ffe6aa67026a82
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2024-01-01 01:24:15 +0100

    feat: User page, edit profile, error handling

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
+from collections.abc import Mapping, Sequence
+from typing import Any
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, EmailField, TextAreaField, BooleanField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 import sqlalchemy as sa
 
 from app import db
@@ -16,13 +18,13 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
-        user = db.session.scalar(sa.select(User).where(User.email == username.data))
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
         if user is not None:
             raise ValidationError('Please use a different username.')
     
@@ -30,3 +32,14 @@ class RegistrationForm(FlaskForm):
         user = db.session.scalar(sa.select(User).where(User.email == email.data))
         if user is not None:
             raise ValidationError('Please use a different email.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        if user is not None:
+            raise ValidationError('Username already taken')
