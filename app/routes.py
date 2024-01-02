aba46c13012c72237e138ffbca74d63745f853ba
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2024-01-02 10:16:41 +0100

    feat: Pagination

--- app/routes.py
+++ app/routes.py
@@ -30,8 +30,23 @@ def home():
         db.session.commit()
         flash("Your post is not live!")
         return redirect(url_for("home"))
-    posts = db.session.scalars(current_user.following_posts).all()
-    return render_template("index.html", title="Home Page", form=form, posts=posts)
+    page = request.args.get("page", 1, type=int)
+    posts = db.paginate(
+        current_user.following_posts,
+        page=page,
+        per_page=app.config["POSTS_PER_PAGE"],
+        error_out=False,
+    )
+    next_url = url_for("home", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("home", page=posts.prev_num) if posts.has_prev else None
+    return render_template(
+        "index.html",
+        title="Home Page",
+        form=form,
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -79,10 +94,23 @@ def register():
 @app.route("/user/<username>")
 @login_required
 def user(username):
-    form = EmptyForm()
     user = db.first_or_404(sa.select(User).where(User.username == username))
-    posts = db.session.scalars(user.posts.select()).all()
-    return render_template("user.html", user=user, posts=posts, form=form)
+    page = request.args.get("page", 1, type=int)
+    query = user.posts.select().order_by(Post.timestamp.desc())
+    posts = db.paginate(
+        query, page=page, per_page=app.config["POSTS_PER_PAGE"], error_out=False
+    )
+    next_url = url_for("home", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("home", page=posts.prev_num) if posts.prev_num else None
+    form = EmptyForm()
+    return render_template(
+        "user.html",
+        user=user,
+        posts=posts,
+        form=form,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
 
 
 @app.route("/edit_profile", methods=["GET", "POST"])
@@ -145,6 +173,13 @@ def unfollow(username):
 @app.route("/explore")
 @login_required
 def explore():
+    page = request.args.get("page", 1, type=int)
     query = sa.select(Post).order_by(Post.timestamp.desc())
-    posts = db.session.scalars(query).all()
-    return render_template("index.html", title="Explore", posts=posts)
+    posts = db.paginate(
+        query, page=page, per_page=app.config["POSTS_PER_PAGE"], error_out=False
+    )
+    next_url = url_for("home", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("home", page=posts.prev_num) if posts.has_prev else None
+    return render_template(
+        "index.html", title="Explore", posts=posts, next_url=next_url, prev_url=prev_url
+    )
--- config.py
+++ config.py
@@ -4,3 +4,4 @@ from os import getenv
 class Config:
     SECRET_KEY = getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = getenv("DATABASE_URI") or "sqlite:///microblog.db"
+    POSTS_PER_PAGE = 25
